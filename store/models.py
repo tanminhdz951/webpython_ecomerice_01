311084cb19a0e68855cd3ca3843a9d89ec2e9b54
Tan Minh <minhtandz951@gmail.com>
2022-10-31 16:10:19 +0700

    firt commit

---
+++ store/models.py
@@ -0,0 +1,55 @@
+from django.urls import reverse
+from django.db import models
+from category.models import Category
+
+# Create your models here.
+
+class Product(models.Model):
+    product_name    = models.CharField(max_length = 200, unique = True)
+    slug            = models.CharField(max_length = 200, unique = True)
+    description     = models.TextField(max_length = 1000, blank = True)
+    price           = models.FloatField()
+    images          = models.ImageField(upload_to = 'photos/products')
+    stock           = models.IntegerField()
+    is_available    = models.BooleanField(default= True)
+    category        = models.ForeignKey(Category, on_delete = models.CASCADE)
+    created_date     = models.DateTimeField(auto_now_add = True)
+    modified_date   = models.DateTimeField(auto_now = True)
+    
+    def __str__(self) :
+        return self.product_name
+
+    def get_url(self):
+        return reverse('product_detail', args = [self.category.slug, self.slug] )
+            
+    @property 
+    def imageURL(self):
+        try:
+            url = self.images.url 
+        except:
+            url = ''
+        return url
+    
+    
+    
+class VariationManager(models.Manager):
+    def colors(self):
+        return super(VariationManager, self).filter(variation_category='color', is_active = True)
+    
+    def sizes(self):
+        return super(VariationManager, self).filter(variation_category='size', is_active = True)
+
+variation_category_choice=(
+    ('color', 'color'),
+    ('size', 'size'),
+)
+class Variation(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    variation_category = models.CharField(max_length=100, choices=variation_category_choice)
+    variation_value = models.CharField(max_length= 100)
+    is_active   = models.BooleanField()
+    created_date = models.DateTimeField(auto_now_add = True)
+    objects = VariationManager()
+    
+    def __str__(self) :
+        return self.variation_value
